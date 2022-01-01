6481d4007bf41e6d027d203a7c6f03de82eca03d
giridhar001 <giridharjha666@gmail.com>
2022-01-01 23:00:20 +0530

    removed commented code

--- todoProject/static/js/todo.js
+++ todoProject/static/js/todo.js
@@ -17,8 +17,6 @@ function saveTodo(){
 				"content-type": "application/json",
 				"accept": "application/json",
 				"authorization": "Bearer 1/1201595394521739:35c0953ed323faee95235e7885dbfdf3",
-				// "cache-control": "no-cache",
-				// "postman-token": "3ba6961a-b3fd-a330-56b3-2a693fb6246e"
 				},
              	'data':JSON.stringify({
              		'data' : data
@@ -27,7 +25,7 @@ function saveTodo(){
              	success:function(response){
              		console.log(response)
              		$('#todo-name').val('')
-             		// location.reload();
+             		location.reload();
              	},
              	
              })
@@ -62,8 +60,6 @@ function editTodo(gid){
 				"content-type": "application/json",
 				"accept": "application/json",
 				"authorization": "Bearer 1/1201595394521739:35c0953ed323faee95235e7885dbfdf3",
-				// "cache-control": "no-cache",
-				// "postman-token": "3ba6961a-b3fd-a330-56b3-2a693fb6246e"
 				},
              	'data':JSON.stringify({
              		'data' : data
@@ -72,7 +68,7 @@ function editTodo(gid){
              	success:function(response){
              		console.log(response)
              		$('#edit-todo-name').val('')
-             		// location.reload();
+             		location.reload();
              	},
              	
              })
@@ -89,13 +85,11 @@ function deleteTodo(gid){
 				"content-type": "application/json",
 				"accept": "application/json",
 				"authorization": "Bearer 1/1201595394521739:35c0953ed323faee95235e7885dbfdf3",
-				// "cache-control": "no-cache",
-				// "postman-token": "3ba6961a-b3fd-a330-56b3-2a693fb6246e"
 				},
              	'contentType': 'application/json; charset=utf-8',
              	success:function(response){
              		console.log(response)
-             		// location.reload();
+             		location.reload();
              	},
              	
              })
